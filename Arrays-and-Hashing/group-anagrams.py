8e64f0ceac5d9fd522db2cd588db197df2365474
joaofam <joaopereira2213@gmail.com>
2023-07-07 12:28:16 +0100

    feat: group-anagrams solution

---
+++ Arrays-and-Hashing/group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        d = {}
+        for word in strs:
+            temp = ''.join(sorted(word))
+            if temp in d:
+                d[temp].append(word)
+            else:
+                d[temp] = [word]
+        return d.values()
+
